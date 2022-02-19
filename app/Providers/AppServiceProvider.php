2722f188e52a0c311708cff95f660da4c0460931
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-02-20 01:49:04 +0900

    always to added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('local')) {
+            Mail::alwaysTo('monzurul.ce.buet@gmail.com');
+        }
     }
 }
